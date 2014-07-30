d978c79b6389070be99898d91236b1a4ec155ad4
Eugene Howe <eugene@xtreme-computers.net>
2014-07-30 14:30:38 -0400

    make ditialocean better

--- lib/fog/digitalocean/models/compute/flavor.rb
+++ lib/fog/digitalocean/models/compute/flavor.rb
@@ -6,6 +6,11 @@ module Fog
       class Flavor < Fog::Model
         identity  :id
         attribute :name
+        attribute :memory, type: :integer
+        attribute :cpu,    type: :integer
+        attribute :disk
+        attribute :cost_per_month
+        attribute :cost_per_hour
       end
     end
   end
